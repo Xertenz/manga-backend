8a2790dd88db771bd958cd0eedef1e60eb81d8a7
Xertenz <ahmad.computer@ymail.com>
2026-06-29 07:34:17 +0300

    feat: secure chapter upload route with sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ChapterController;
 use App\Http\Controllers\Api\MangaController;
 use Illuminate\Http\Request;
@@ -9,6 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+/*
 Route::apiResource('mangas', MangaController::class);
 Route::apiResource('chapters', ChapterController::class);
+*/
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('mangas', [MangaController::class, 'index']);
+Route::get('mangas/{manga}', [MangaController::class, 'show']);
+
+Route::get('/chapters', [ChapterController::class, 'index']);
+Route::get('/chapters/{chapter}', [ChapterController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/chapters', [ChapterController::class, 'store']);
+});
